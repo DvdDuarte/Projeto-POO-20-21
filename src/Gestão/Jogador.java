81023fe6fae4ea554e8a1ac62e4653494c2e21c1
sdasl03 <sdaslira@gmail.com>
2021-03-16 09:16:53 +0000

    getters and setters do jogador

--- src/Gestão/Jogador.java
+++ src/Gestão/Jogador.java
@@ -15,5 +15,87 @@ public class Jogador {
     private double capacidadePasse;
     private List historial;
 
+    public Jogador() {
+        //completar
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getRemate() {
+        return remate;
+    }
+
+    public void setRemate(double remate) {
+        this.remate = remate;
+    }
+
+    public double getVelocidade() {
+        return velocidade;
+    }
+
+    public double getResistencia() {
+        return resistencia;
+    }
+
+    public void setResistencia(double resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    public void setVelocidade(double velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public double getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(double destreza) {
+        this.destreza = destreza;
+    }
+
+    public double getImpulsao() {
+        return impulsao;
+    }
+
+    public void setImpulsao(double impulsao) {
+        this.impulsao = impulsao;
+    }
+
+    public double getJogoCabeca() {
+        return jogoCabeca;
+    }
+
+    public void setJogoCabeca(double jogoCabeca) {
+        this.jogoCabeca = jogoCabeca;
+    }
+
+    public double getCapacidadePasse() {
+        return capacidadePasse;
+    }
+
+    public void setCapacidadePasse(double capacidadePasse) {
+        this.capacidadePasse = capacidadePasse;
+    }
+
+    public List getHistorial() {
+        return historial;
+    }
+
+    public void setHistorial(List historial) {
+        this.historial = historial;
+    }
 }

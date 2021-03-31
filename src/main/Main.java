448dfaed8e4b2fdda681c00ae88bcc2b31f9d1f9
sdasl03 <sdaslira@gmail.com>
2021-03-31 16:55:31 +0100

    "encomenda" de funcoes sobre equipas e jogadores, ver TripleSfootball.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-     //   Jogador j = new Jogador("olá",12, new ArrayList<>());
+    //  Jogador j = new Jogador("olá",12, new ArrayList<>());
         TripleSFootball tsf = new TripleSFootball();
         Scanner in = new Scanner(System.in);
         int op = 0;
@@ -28,26 +28,78 @@ alterar nome da equipa
 apagar jogador(apenas os criados)
 
  */
+/*
+sugestoes de interfaces:
+1- de edicoes:
+criar equipaas e/ou jogadores, transferir joagdores, apagar jogadores e/ou equipas criadas
+2- de jogar:
+    inicialisar:
+        inserir nome do tecnico, inserir equipa a ser utilizada,
+    funcionalidades:
+        basicas:
+            alterar escalacao antes do jogo, titulares e reservas
+            fazer max de 3 substituicoes durante o intervalo
+            jogar as partidas
+        avancadas:
+            contratar e/ou despedir jogador
 
+
+ */
         do {
-            System.out.println("1 - Criar equipa\n2 - Criar Jogador\n3 - Saír");
+            System.out.println("1- Criar equipa\n2- Criar Jogador\n3- Tranferir jogador de clube\n4- Apagar jogador criado\n" +
+                    "5- Apagar equipa\n0- Saír");
             op = in.nextInt();
 
             switch(op){
                 case 1 :
+                    //criar equipa
                     System.out.println("Insira o nome da Equipajogador a ser criado");
-                    String nomeE= in.nextLine();
+                    String nomeE1= in.nextLine();
                     System.out.println("Insira a idade do jogador a ser criado");
+                    tsf.criaEquipa();//com os argumentos certos
+
+                    break;
 
                 case 2 :
+                    //criar jogador
                     System.out.println("Insira o nome do jogador a ser criado");
                     String nomeJ= in.nextLine();
                     System.out.println("Insira a idade do jogador a ser criado");
-                    int idadeJ=in.nextInt();
+                    int idadeJ2=in.nextInt();
                     System.out.println("Insira resto das informacoes do jogador a ser criado");
                     tsf.criaJogador();//com os argumentos certos
+
+                    break;
+                case 3 :
+                    //transferir jogador
+                    System.out.println("Insira o nome do jogador a ser transferido");
+                    String nomeJ3= in.nextLine();
+                    //caso o jogador estiver na equipa fantasma, sai dela para a outra equipa, caso contrario
+                    System.out.println("Insira o nome da nova equipa do jogador, " +
+                        "caso o jogador nao va para nenhuma equipa ainda, digite 'fantasma'");
+                    String nomeE3_nova= in.nextLine();
+                    tsf.transfereJogador(nomeJ3,nomeE3_nova);
+
+                    break;
+                case 4 :
+                    //apagar jogador
+                    System.out.println("Insira o nome do jogador a ser apagado");
+                    String nomeJ4= in.nextLine();
+                    //caso o jogador nao tiver a equipa fantasma como primeiro clube, nao pode ser apagado
+
+                    break;
+                case 5:
+                    //apagar equipa
+                    System.out.println("Insira o nome da equipa a ser apagado");
+                    String nomeE5= in.nextLine();
+                    //caso nao for uma equipa criada nao pode ser apagada
+
+                default :
+
+
+                    break;
             }
 
-        }while(op != 3);
+        }while(op != 0);
     }
 }
--- src/main/TripleSFootball.java
+++ src/main/TripleSFootball.java
@@ -4,23 +4,54 @@ import gestão.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class TripleSFootball {
-   // private List<Equipa> equipas;
-  //  private List<Jogador> jogadores;
+  private List<Equipa> equipas;
+  //resto das propriedades do jogo por definir
+
+    //private List<Jogador> jogadores;
     //lista de equipas
     //lista de jogadores
 
+
+    //tambem criar equipa fantasma
     public void criaEquipa(){
+        //rever argumentos
+        //Equipa e1= new Equipa();
+
 
     }
-    /*Jogador j1=new Jogador("nome1",25, new ArrayList<>());
-                    j.imprimeJogador(j1);*/
+
 
     public void criaJogador(){
-        //rever
+        //rever argumentos
+
         //Jogador j1= new Jogador();
+
+    }
+    public void transfereJogador(String nomeJ, String nomeENova){
+        //usa os nomes para identificar os objetos Jogador j, Equipa eAntiga e Equipa eNova
+
+
+        //retira jogador da lista de jogadores da equipa eAntiga
+
+
+        //acrescenta jogador a lista de jogadores da equipa eNova
+
+
+        //acrescenta equipa eNova ao final do historial do jogador j
+
+    }
+    public void apagaJogador(String nomeJ){
+        //usa o nome para identificar o objeto Jogador j
+
+
+        //caso jogador j esteja na equipa fantasma
+    }
+    public void apagaEquipa(String nomeE){
+      //usa o nome para identificar o objeto Equipa E
+
+      //caso a Equipa E tenha sido criada pelo utilizador
     }
 
 

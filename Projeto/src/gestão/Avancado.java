93182f1f695177c0ed9b02701503696cb6495b02
Jarvas <daniel.faria@outlook.pt>
2021-06-12 22:45:34 +0100

    final

--- Projeto/src/gestão/Avancado.java
+++ Projeto/src/gestão/Avancado.java
@@ -2,33 +2,28 @@ package gestão;
 
 import java.util.List;
 
-public class Defesa extends Jogador {
+public class Avancado extends Jogador {
 
-    private final double pVelocidade = 0.08;
-    private final double pResistencia = 0.11;
-    private final double pDestreza = 0.07;
-    private final double pImpulsao = 0.13;
-    private final double pJogoDeCabeca = 0.13;
-    private final double pRemate = 0.07;
-    private final double pCapacidadeDePasse = 0.11;
+    private final double pVelocidade = 0.12;
+    private final double pResistencia = 0.10;
+    private final double pDestreza = 0.10;
+    private final double pImpulsao = 0.15;
+    private final double pJogoDeCabeca = 0.18;
+    private final double pRemate = 0.20;
+    private final double pCapacidadeDePasse = 0.15;
 
 
-
-    public Defesa() {
+    public Avancado() {
         super();
     }
 
-    public Defesa(String n, int nc, int vel, int res, int des, int imp, int jdc, int rem, int cdp) {
+    public Avancado(String n, int nc, int vel, int res, int des, int imp, int jdc, int rem, int cdp) {
         super(n, nc, vel, res, des, imp, jdc, rem, cdp);
     }
 
-    public Defesa(Defesa d) {
-        super(d);
-    }
-
-    public static Defesa parse(String input){
+    public static Avancado parse(String input){
         String[] campos = input.split(",");
-        return new Defesa(campos[0], Integer.parseInt(campos[1]),
+        return new Avancado(campos[0], Integer.parseInt(campos[1]),
                 Integer.parseInt(campos[2]),
                 Integer.parseInt(campos[3]),
                 Integer.parseInt(campos[4]),
@@ -39,7 +34,7 @@ public class Defesa extends Jogador {
     }
 
     public String jogadorParaLinha(){
-        return "Defesa:"+getNome()
+        return "Avancado:"+getNome()
                 +","+getCamisola()
                 +","+getVelocidade()
                 +","+getResistencia()
@@ -48,6 +43,10 @@ public class Defesa extends Jogador {
                 +","+getJogoDeCabeca()
                 +","+getRemate()
                 +","+getCapacidadeDePasse();
+    }
+
+    public Avancado(Avancado a) {
+        super(a);
     }
 
     public double getPesoVelocidade(){
@@ -81,10 +80,10 @@ public class Defesa extends Jogador {
     public double calculaHabilidade(){
         double habilidade;
 
-        habilidade = super.getRemate()*this.pRemate + super.getCapacidadeDePasse()*this.pCapacidadeDePasse
-                + super.getDestreza()*this.pDestreza + super.getImpulsao()*this.pImpulsao
-                + super.getJogoDeCabeca()*this.pJogoDeCabeca + super.getResistencia()*this.pResistencia
-                + super.getVelocidade()*this.pVelocidade;
+            habilidade = super.getRemate()*this.pRemate + super.getCapacidadeDePasse()*this.pCapacidadeDePasse
+                    + super.getDestreza()*this.pDestreza + super.getImpulsao()*this.pImpulsao
+                    + super.getJogoDeCabeca()*this.pJogoDeCabeca + super.getResistencia()*this.pResistencia
+                    + super.getVelocidade()*this.pVelocidade;
 
         return habilidade;
     }
@@ -97,18 +96,17 @@ public class Defesa extends Jogador {
         if(o == this) return true;
         if(o.getClass() != this.getClass() || o == null) return false;
 
-        Defesa d = (Defesa)o;
-        return super.equals(o) && d.getPesoVelocidade() == this.pVelocidade
-                && d.getPesoCapacidadeDePasse() == this.pCapacidadeDePasse
-                && d.getPesoDestreza() == this.pDestreza
-                && d.getPesoImpulsao() == this.pImpulsao
-                && d.getPesoJogoDeCabeca() == this.pJogoDeCabeca
-                && d.getPesoRemate() == this.pRemate
-                && d.getPesoResistencia() == this.pResistencia;
+        Avancado a = (Avancado)o;
+        return super.equals(o) && a.getPesoVelocidade() == this.pVelocidade
+                && a.getPesoCapacidadeDePasse() == this.pCapacidadeDePasse
+                && a.getPesoDestreza() == this.pDestreza
+                && a.getPesoImpulsao() == this.pImpulsao
+                && a.getPesoJogoDeCabeca() == this.pJogoDeCabeca
+                && a.getPesoRemate() == this.pRemate
+                && a.getPesoResistencia() == this.pResistencia;
     }
 
-    public Defesa clone() {
-        return new Defesa(this);
+    public Avancado clone() {
+        return new Avancado(this);
     }
-
 }
--- Projeto/src/gestão/Defesa.java
+++ Projeto/src/gestão/Defesa.java
@@ -2,28 +2,33 @@ package gestão;
 
 import java.util.List;
 
-public class Avancado extends Jogador {
+public class Defesa extends Jogador {
 
-    private final double pVelocidade = 0.11;
-    private final double pResistencia = 0.08;
-    private final double pDestreza = 0.08;
-    private final double pImpulsao = 0.1;
-    private final double pJogoDeCabeca = 0.13;
-    private final double pRemate = 0.15;
-    private final double pCapacidadeDePasse = 0.08;
+    private final double pVelocidade = 0.08;
+    private final double pResistencia = 0.22;
+    private final double pDestreza = 0.12;
+    private final double pImpulsao = 0.16;
+    private final double pJogoDeCabeca = 0.20;
+    private final double pRemate = 0.10;
+    private final double pCapacidadeDePasse = 0.12;
 
 
-    public Avancado() {
+
+    public Defesa() {
         super();
     }
 
-    public Avancado(String n, int nc, int vel, int res, int des, int imp, int jdc, int rem, int cdp) {
+    public Defesa(String n, int nc, int vel, int res, int des, int imp, int jdc, int rem, int cdp) {
         super(n, nc, vel, res, des, imp, jdc, rem, cdp);
     }
 
-    public static Avancado parse(String input){
+    public Defesa(Defesa d) {
+        super(d);
+    }
+
+    public static Defesa parse(String input){
         String[] campos = input.split(",");
-        return new Avancado(campos[0], Integer.parseInt(campos[1]),
+        return new Defesa(campos[0], Integer.parseInt(campos[1]),
                 Integer.parseInt(campos[2]),
                 Integer.parseInt(campos[3]),
                 Integer.parseInt(campos[4]),
@@ -34,7 +39,7 @@ public class Avancado extends Jogador {
     }
 
     public String jogadorParaLinha(){
-        return "Avancado:"+getNome()
+        return "Defesa:"+getNome()
                 +","+getCamisola()
                 +","+getVelocidade()
                 +","+getResistencia()
@@ -43,10 +48,6 @@ public class Avancado extends Jogador {
                 +","+getJogoDeCabeca()
                 +","+getRemate()
                 +","+getCapacidadeDePasse();
-    }
-
-    public Avancado(Avancado a) {
-        super(a);
     }
 
     public double getPesoVelocidade(){
@@ -80,10 +81,10 @@ public class Avancado extends Jogador {
     public double calculaHabilidade(){
         double habilidade;
 
-            habilidade = super.getRemate()*this.pRemate + super.getCapacidadeDePasse()*this.pCapacidadeDePasse
-                    + super.getDestreza()*this.pDestreza + super.getImpulsao()*this.pImpulsao
-                    + super.getJogoDeCabeca()*this.pJogoDeCabeca + super.getResistencia()*this.pResistencia
-                    + super.getVelocidade()*this.pVelocidade;
+        habilidade = super.getRemate()*this.pRemate + super.getCapacidadeDePasse()*this.pCapacidadeDePasse
+                + super.getDestreza()*this.pDestreza + super.getImpulsao()*this.pImpulsao
+                + super.getJogoDeCabeca()*this.pJogoDeCabeca + super.getResistencia()*this.pResistencia
+                + super.getVelocidade()*this.pVelocidade;
 
         return habilidade;
     }
@@ -96,17 +97,18 @@ public class Avancado extends Jogador {
         if(o == this) return true;
         if(o.getClass() != this.getClass() || o == null) return false;
 
-        Avancado a = (Avancado)o;
-        return super.equals(o) && a.getPesoVelocidade() == this.pVelocidade
-                && a.getPesoCapacidadeDePasse() == this.pCapacidadeDePasse
-                && a.getPesoDestreza() == this.pDestreza
-                && a.getPesoImpulsao() == this.pImpulsao
-                && a.getPesoJogoDeCabeca() == this.pJogoDeCabeca
-                && a.getPesoRemate() == this.pRemate
-                && a.getPesoResistencia() == this.pResistencia;
+        Defesa d = (Defesa)o;
+        return super.equals(o) && d.getPesoVelocidade() == this.pVelocidade
+                && d.getPesoCapacidadeDePasse() == this.pCapacidadeDePasse
+                && d.getPesoDestreza() == this.pDestreza
+                && d.getPesoImpulsao() == this.pImpulsao
+                && d.getPesoJogoDeCabeca() == this.pJogoDeCabeca
+                && d.getPesoRemate() == this.pRemate
+                && d.getPesoResistencia() == this.pResistencia;
     }
 
-    public Avancado clone() {
-        return new Avancado(this);
+    public Defesa clone() {
+        return new Defesa(this);
     }
+
 }
